706b2b20fa2aa9ba0e4eb9f2fcfcc6c405ab40e1
veeelasco <caaarlosvelasco@gmail.com>
2024-04-23 18:09:52 +0200

    registro usuario

--- src/main/java/controlador/LoginController.java
+++ src/main/java/controlador/LoginController.java
@@ -26,207 +26,226 @@ import java.io.IOException;
  */
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 	@SuppressWarnings("unchecked")
-	protected void procesarPeticion(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+	protected void procesarPeticion(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		response.setContentType("text/html;charset=UTF-8");
-		String nombreUsuario="",password="",passwordRe="",email = "hola";
-		String token="";
-		
-		String codVerificacion="";
+		String nombreUsuario = "", password = "", passwordRe = "", email = "hola";
+		String token = "";
+
+		String codVerificacion = "";
 		String operacion = request.getParameter("opcion");
-		if (operacion == null){
+		if (operacion == null) {
 			operacion = "";
 		}
-		
+
 		RequestDispatcher rd;
-		
-		
-		switch(operacion){
-		
+
+		switch (operacion) {
+
 		case "registrarse": // es el servidor quien redirije internamente hacia la vista registrarse
-		    rd = request.getRequestDispatcher("registro.jsp");
-		    rd.forward(request, response);
-		    break;
-		case "registrarNuevoUsuario"://Se realiza el insert de un nuevo usuario en la base de datos 
-			nombreUsuario= request.getParameter("nombreUsuario");
-			password= request.getParameter("contrasenia");
-			passwordRe= request.getParameter("contraseniaRe");
-			email= request.getParameter("correoUsuario");
-			String extensionCorreo=email.substring(email.length()-10, email.length());
-			EntityManager em1= modelo.HibernateUtils.getEmf().createEntityManager();
-		
-			Query q1= em1.createQuery("FROM Usuario");
-			List<Usuario> usuarios = q1.getResultList(); 
-			for(Usuario user:usuarios) {
-				if(user.getEmail().compareTo(email)==0) {
-					request.setAttribute("error", "El correo electronico ya esta en uso");
-	        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-				}else if (user.getNombre().compareTo(nombreUsuario)==0) {
-					request.setAttribute("error", "El nombre de usuario ya esta en uso");
-	        		request.getRequestDispatcher("registro.jsp").forward(request, response);
+			rd = request.getRequestDispatcher("registro.jsp");
+			rd.forward(request, response);
+			break;
+		case "registrarNuevoUsuario":// Se realiza el insert de un nuevo usuario en la base de datos
+
+			nombreUsuario = request.getParameter("nombreUsuario");
+			password = request.getParameter("contrasenia");
+			passwordRe = request.getParameter("contraseniaRe");
+			email = request.getParameter("correoUsuario");
+			String extensionCorreo = email.substring(email.length() - 10, email.length());
+			EntityManager em1 = modelo.HibernateUtils.getEmf().createEntityManager();
+			try {
+				Query q1 = em1.createQuery("FROM Usuario");
+				List<Usuario> usuarios = q1.getResultList();
+				for (Usuario user : usuarios) {
+
+					if (user.getEmail().compareTo(email) == 0) {
+						request.setAttribute("error", "El correo electronico ya esta en uso");
+						request.getRequestDispatcher("registro.jsp").forward(request, response);
+					} else if (user.getNombre().compareTo(nombreUsuario) == 0) {
+						request.setAttribute("error", "El nombre de usuario ya esta en uso");
+						request.getRequestDispatcher("registro.jsp").forward(request, response);
+					}
+
 				}
+				if ((password.compareTo(passwordRe) != 0)) {
+					request.setAttribute("error", "Las contraseñas debe coincidir");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (extensionCorreo.compareTo("@gmail.com") != 0) {
+					System.out.println(email.substring(email.length() - 10, email.length()));
+					request.setAttribute("error", "Correo no valido");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (!(password.matches(".*[A-Z].*"))) {
+					request.setAttribute("error", "La contraseña debe contener como mínimo una mayuscula");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (!(password.matches(".*[0-9].*"))) {
+					request.setAttribute("error", "La contraseña debe contener un numero");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (nombreUsuario.matches(".*[!¡@#$%^&*()¿?¬~].*")) {
+					request.setAttribute("error", "El nombre de usuario no debe contener un caracter especial");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (nombreUsuario.length() < 3) {
+					request.setAttribute("error", "El nombre de usuario debe ser superior a 3 caracteres");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if (!(password.matches(".*[!¡@#$%^&*()¿?¬~].*"))) {
+					request.setAttribute("error", "La contraseña debe contener como mínimo un caracter especial");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				} else if ((password.length() <= 8) || (password.length() > 20)) {
+					request.setAttribute("error",
+							"La contraseña no puede ser inferior a los 8 caracteres ni superior a los 20");
+					request.getRequestDispatcher("registro.jsp").forward(request, response);
+				}
+
+				// Try catch exceptiones java.lang.IllegalStateException
+				else {
+					// Guardar los datos del usuario y el token de confirmación en la sesión del
+					// usuario
+					request.getSession().setAttribute("nombreUsuario", nombreUsuario);
+					request.getSession().setAttribute("password", password);
+					request.getSession().setAttribute("email", email);
+					System.out.println(nombreUsuario + " " + password + " " + email);
+					token = generarToken();
+					System.out.println("Token creado: " + token);
+					EmailValidator.enviarCorreo(email, token);
+					request.getSession().setAttribute("token", token);
+					System.out.println("Correo enviado");
+					// Redirigir a una página para que el usuario confirme su correo electrónico
+					// request.setAttribute("token", token);
+					request.getRequestDispatcher("confirmar_correo.jsp").forward(request, response);
+
+				}
+			} catch (IllegalStateException e) {
+				System.out.println(e.getMessage());
 			}
-			
-			if((password.compareTo(passwordRe)!=0)) {
-				request.setAttribute("error", "Las contraseñas debe coincidir");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(extensionCorreo.compareTo("@gmail.com")!=0){
-				System.out.println(email.substring(email.length()-10, email.length()));
-				request.setAttribute("error", "Correo no valido");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(!(password.matches(".*[A-Z].*"))) {
-				request.setAttribute("error", "La contraseña debe contener como mínimo una mayuscula");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(!(password.matches(".*[0-9].*"))){
-				request.setAttribute("error", "La contraseña debe contener un numero");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(nombreUsuario.matches(".*[!¡@#$%^&*()¿?¬~].*")) {
-				request.setAttribute("error", "El nombre de usuario no debe contener un caracter especial");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(nombreUsuario.length()<3) {
-				request.setAttribute("error", "El nombre de usuario debe ser superior a 3 caracteres");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}else if(!(password.matches(".*[!¡@#$%^&*()¿?¬~].*"))) {
-				request.setAttribute("error", "La contraseña debe contener como mínimo un caracter especial");
-        		request.getRequestDispatcher("registro.jsp").forward(request, response);
-			}
-			//Try catch exceptiones java.lang.IllegalStateException
-			else {
-		    // Guardar los datos del usuario y el token de confirmación en la sesión del usuario
-		    request.getSession().setAttribute("nombreUsuario", nombreUsuario);
-		    request.getSession().setAttribute("password", password);
-		    request.getSession().setAttribute("email", email);
-		    System.out.println(nombreUsuario + " " + password + " " + email);
-		    token = generarToken();
-		    System.out.println("Token creado: " + token);
-		    EmailValidator.enviarCorreo(email, token);
-		    request.getSession().setAttribute("token",token);
-		    System.out.println("Correo enviado");
-		 // Redirigir a una página para que el usuario confirme su correo electrónico
-		    //request.setAttribute("token", token);
-		    request.getRequestDispatcher("confirmar_correo.jsp").forward(request, response);
-		}break;
+			break;
 		case "verificarCorreo":
 			codVerificacion = request.getParameter("cod_verificacion");
 			nombreUsuario = (String) request.getSession().getAttribute("nombreUsuario");
 			password = (String) request.getSession().getAttribute("password");
 			email = (String) request.getSession().getAttribute("email");
-			token=(String) request.getSession().getAttribute("token");
-			System.out.println(nombreUsuario+" "+password+" "+email);
+			token = (String) request.getSession().getAttribute("token");
+			System.out.println(nombreUsuario + " " + password + " " + email);
 			System.out.println("TokenDef: " + token);
 			System.out.println("codVeri: " + codVerificacion);
 			if (token.compareTo(codVerificacion) != 0) {
-			    request.setAttribute("token", token);
-			    request.setAttribute("email", email);
-			    request.setAttribute("error", "Codigo de verificacion incorrecto");
-			    request.getRequestDispatcher("confirmar_correo.jsp").forward(request, response);
+				request.setAttribute("token", token);
+				request.setAttribute("email", email);
+				request.setAttribute("error", "Codigo de verificacion incorrecto");
+				request.getRequestDispatcher("confirmar_correo.jsp").forward(request, response);
 			} else {
-			    Usuario nuevoUsuario = new Usuario(nombreUsuario, password, email);
-			    System.out.println(nuevoUsuario);
-			    EntityManager em2 = HibernateUtils.getEmf().createEntityManager();
-			    EntityTransaction transacion = em2.getTransaction();
-			    try {
-			        transacion.begin();
-			        em2.persist(nuevoUsuario);
-			        transacion.commit();
-			        System.out.println("Usuario Subido");
-			    } catch (Exception e) {
-			        System.err.println(e.getMessage());
-			        transacion.rollback();
-			    } finally {
-			        em2.close();
-			    }
-			    response.sendRedirect("login.jsp");
+				Usuario nuevoUsuario = new Usuario(nombreUsuario, password, email);
+				System.out.println(nuevoUsuario);
+				EntityManager em2 = HibernateUtils.getEmf().createEntityManager();
+				EntityTransaction transacion = em2.getTransaction();
+				try {
+					transacion.begin();
+					em2.persist(nuevoUsuario);
+					transacion.commit();
+					System.out.println("Usuario Subido");
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+					transacion.rollback();
+				} finally {
+					em2.close();
+				}
+				response.sendRedirect("login.jsp");
 			}
 			break;
 
 		case "perfil": // es el cliente quien deber� invocar a este recurso
-			response.sendRedirect("perfilUsuario.jsp"); 
+			response.sendRedirect("perfilUsuario.jsp");
 			break;
 		case "NuevoViaje": // es el cliente quien invoca este recurso al presionar el oton de Crear Viaje
-			response.sendRedirect("nuevoViaje.jsp"); 
-			break; 
+			response.sendRedirect("nuevoViaje.jsp");
+			break;
 		case "iniciarSesion":
-			// Obtén los valores de los campos de nombre de usuario y contraseña del formulario
-            email = request.getParameter("correoUsuario");
-            password = request.getParameter("contrasenia");
-   
-            // Realiza la verificación con la base de datos
-            EntityManager em = HibernateUtils.getEmf().createEntityManager();
-            Query consulta = em.createQuery("SELECT u FROM Usuario u WHERE u.email = :correo");
-            consulta.setParameter("correo", email);
-            try {
-            	Usuario usuario = (Usuario) consulta.getSingleResult();
-            	
-            	if (usuario != null && usuario.getContrasenia().equals(password)) {
-            	    // Almacena los datos del usuario en la sesión
-            	    HttpSession session = request.getSession();
-            	    session.setAttribute("usuario", usuario);
-            	    // Redirige a la página de perfil
-            	    response.sendRedirect("perfilUsuario.jsp");
-            	} else {
-            	    // Si las credenciales no son válidas, redirige de nuevo al formulario de inicio de sesión con un mensaje de error
-            	    request.setAttribute("error", "Nombre de usuario o contraseña incorrectos");
-            	    request.getRequestDispatcher("login.jsp").forward(request, response);
-            	}
-            	
-            }catch(NoResultException e) {
-            	request.setAttribute("error", "Nombre de usuario o contraseña incorrectas");
-            	request.getRequestDispatcher("login.jsp").forward(request, response);
-            }finally{
-            	em.close();
-            }
-            break;
-         
-		 /* case "borrarActor": // es el servidor quien redirije
-		 * internamente hacia la vista procNuevoActor.jsp rd =
+			// Obtén los valores de los campos de nombre de usuario y contraseña del
+			// formulario
+			email = request.getParameter("correoUsuario");
+			password = request.getParameter("contrasenia");
+
+			// Realiza la verificación con la base de datos
+			EntityManager em = HibernateUtils.getEmf().createEntityManager();
+			Query consulta = em.createQuery("SELECT u FROM Usuario u WHERE u.email = :correo");
+			consulta.setParameter("correo", email);
+			try {
+				Usuario usuario = (Usuario) consulta.getSingleResult();
+
+				if (usuario != null && usuario.getContrasenia().equals(password)) {
+					// Almacena los datos del usuario en la sesión
+					HttpSession session = request.getSession();
+					session.setAttribute("usuario", usuario);
+					// Redirige a la página de perfil
+					response.sendRedirect("perfilUsuario.jsp");
+				} else {
+					// Si las credenciales no son válidas, redirige de nuevo al formulario de inicio
+					// de sesión con un mensaje de error
+					request.setAttribute("error", "Nombre de usuario o contraseña incorrectos");
+					request.getRequestDispatcher("login.jsp").forward(request, response);
+				}
+
+			} catch (NoResultException e) {
+				request.setAttribute("error", "Nombre de usuario o contraseña incorrectas");
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			} finally {
+				em.close();
+			}
+			break;
+
+		/*
+		 * case "borrarActor": // es el servidor quien redirije internamente hacia la
+		 * vista procNuevoActor.jsp rd =
 		 * request.getRequestDispatcher("borrarActor.jsp"); rd.forward(request,
 		 * response); break;
 		 */
-        
-        
+
 		case "Loger":
 			response.sendRedirect("login.jsp");
-			
+
 			break;
-			
+
 		default:
 			// es el cliente quien deber� invocar a este recurso
 			response.sendRedirect("index.jsp");
-	
+
 		}
-		
+
 	}
-	
+
 	private String generarToken() {
-        // Implementa la lógica para generar un token de confirmación único
-        return UUID.randomUUID().toString();
-    }
-	
+		// Implementa la lógica para generar un token de confirmación único
+		return UUID.randomUUID().toString();
+	}
+
 	/**
-	 * Método contro	lador para las peticiones HTTP GET que delega al método `procesarPeticion`.
+	 * Método controlador para las peticiones HTTP GET que delega al método
+	 * `procesarPeticion`.
 	 *
 	 * @param request  Objeto de solicitud HTTP servlet.
 	 * @param response Objeto de respuesta HTTP servlet.
-	 * @throws IOException      si hay un error de entrada o salida mientras el servlet está manejando la solicitud.
+	 * @throws IOException      si hay un error de entrada o salida mientras el
+	 *                          servlet está manejando la solicitud.
 	 * @throws ServletException si la solicitud GET no se pudo manejar.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  
-	{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		procesarPeticion(request, response);
 	}
 
-	
 	/**
-	 * Método controlador para las peticiones HTTP POST que delega al método `procesarPeticion`.
+	 * Método controlador para las peticiones HTTP POST que delega al método
+	 * `procesarPeticion`.
 	 *
 	 * @param request  Objeto de solicitud HTTP servlet.
 	 * @param response Objeto de respuesta HTTP servlet.
-	 * @throws IOException      si hay un error de entrada o salida mientras el servlet está manejando la solicitud.
+	 * @throws IOException      si hay un error de entrada o salida mientras el
+	 *                          servlet está manejando la solicitud.
 	 * @throws ServletException si la solicitud POST no se pudo manejar.
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  
-	{
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		procesarPeticion(request, response);
 	}
 
--- src/main/webapp/registro.jsp
+++ src/main/webapp/registro.jsp
@@ -18,21 +18,23 @@
 	<form  action="LoginController" method="post">
 		<input type="hidden" name="opcion" value="registrarNuevoUsuario">
 		
-        <label for="nombreUsuario">Nombre de Usuario:</label><br>
+        <label for="nombreUsuario">Nombre de Usuario</label><br>
+        <small>(superior a 3 caracteres)</small><br>
         <input type="text" id="nombreUsuario" name="nombreUsuario" min="3"><br>
-        
-        <label for="contrasenia">Contraseña:</label><br>
+
+        <label for="contrasenia">Contraseña</label><br>
+        <small>(Debe incluir(mínimo un número, un símbolo especial(!?*%) y una mayúscula) y ser superior a 8 caracteres e inferior a 20 caracteres)</small><br>
         <input type="password" id="contrasenia" name="contrasenia"><br><br>
         
-        <label for="contraseniaRe">Confirmar contraseña:</label><br>
+        <label for="contraseniaRe">Confirmar contraseña</label><br>
         <input type="password" id="contraseniaRe" name="contraseniaRe"><br><br>
         
-        <label for="correoUsuario">Correo Electrónico:</label>
+        <label for="correoUsuario">Correo Electrónico</label><br>
+        <small>Solo se permiten correos del dominio (@gmail.com)</small><br>
         <input type="email"  id="correoUsuario" name="correoUsuario" >
        	
-       	<input id="registrarme" type="submit" value="Confirmar"> 
-       	<a href="login.jsp">Volver al inicio</a>
-       	
-	</form>
+       	<input id="registrarme" type="submit" value="Confirmar">
+ 	</form>
+ 	<a href="login.jsp">Volver al inicio</a>
 </body>
 </html>
